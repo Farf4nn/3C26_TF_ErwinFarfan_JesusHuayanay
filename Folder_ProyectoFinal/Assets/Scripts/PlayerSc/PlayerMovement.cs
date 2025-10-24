db8be9d112e2956138331183f566ef0ad366034f
Shinnida <xzjesusxz123@gmail.com>
2025-10-23 23:57:40 -0500

    first commit

---
+++ Folder_ProyectoFinal/Assets/Scripts/PlayerSc/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed = 5f;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
